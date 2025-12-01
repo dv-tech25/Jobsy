74154e4bbfd1a588be7a64fcdaaf363573b037c0
dv-tech25 <devendra8090849216@gmail.com>
2025-12-01 19:00:30 +0530

    set cors

--- backand/index.js
+++ backand/index.js
@@ -5,11 +5,22 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT ;
 
-app.use(cors({
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://your-frontend-domain.com"  
+];
 
-   origin: "http://localhost:5173",
-  credentials: true 
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser()); 
 
